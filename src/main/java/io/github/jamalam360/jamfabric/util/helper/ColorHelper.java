66c4597e358c0a7b78da8698e812b3e31061bec1
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-01-04 23:45:57 +0000

    QM

--- src/main/java/io/github/jamalam360/jamfabric/util/helper/ColorHelper.java
+++ src/main/java/io/github/jamalam360/jamfabric/util/helper/ColorHelper.java
@@ -89,7 +89,7 @@ public class ColorHelper {
         Color[] colors = new Color[image.getWidth() * image.getHeight()];
         for (int x = 0; x < image.getWidth(); x++) {
             for (int y = 0; y < image.getHeight(); y++) {
-                int[] pixelColors = ColorHelper.unpackRgbaColor(image.getColor(x, y));
+                int[] pixelColors = ColorHelper.unpackRgbaColor(image.getPixelColor(x, y));
                 if ((pixelColors[0] != 255 && pixelColors[1] != 255 && pixelColors[2] != 255) && (pixelColors[0] != 0 && pixelColors[1] != 0 && pixelColors[2] != 0)) {
                     colors[x + y] = new Color(pixelColors[0], pixelColors[1], pixelColors[2]);
                 }
