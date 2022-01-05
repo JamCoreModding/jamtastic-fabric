1dedb02d03310b1afc2b4a3164b3e7f824faae8b
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-01-05 14:31:35 +0000

    Add HSB support back into Color.java

--- src/main/java/io/github/jamalam360/jamfabric/util/Color.java
+++ src/main/java/io/github/jamalam360/jamfabric/util/Color.java
@@ -24,8 +24,6 @@
 
 package io.github.jamalam360.jamfabric.util;
 
-import java.awt.image.ColorModel;
-
 /**
  * '''Liberated''' from AWT because that package breaks on macOS.
  */
@@ -59,6 +57,10 @@ public class Color {
         }
     }
 
+    public Color(int rgb) {
+        value = 0xff000000 | rgb;
+    }
+
     public Color(int r, int g, int b) {
         this(r, g, b, 255);
     }
@@ -71,94 +73,105 @@ public class Color {
         testColorValueRange(r, g, b, a);
     }
 
-    /**
-     * Returns the red component in the range 0-255 in the default sRGB
-     * space.
-     *
-     * @return the red component.
-     * @see #getRGB
-     */
     public int getRed() {
         return (getRGB() >> 16) & 0xFF;
     }
 
-    /**
-     * Returns the green component in the range 0-255 in the default sRGB
-     * space.
-     *
-     * @return the green component.
-     * @see #getRGB
-     */
     public int getGreen() {
         return (getRGB() >> 8) & 0xFF;
     }
 
-    /**
-     * Returns the blue component in the range 0-255 in the default sRGB
-     * space.
-     *
-     * @return the blue component.
-     * @see #getRGB
-     */
     public int getBlue() {
         return (getRGB() >> 0) & 0xFF;
     }
 
-    /**
-     * Returns the RGB value representing the color in the default sRGB
-     * {@link ColorModel}.
-     * (Bits 24-31 are alpha, 16-23 are red, 8-15 are green, 0-7 are
-     * blue).
-     *
-     * @return the RGB value of the color in the default sRGB
-     * {@code ColorModel}.
-     * @see java.awt.image.ColorModel#getRGBdefault
-     * @see #getRed
-     * @see #getGreen
-     * @see #getBlue
-     * @since 1.0
-     */
     public int getRGB() {
         return value;
     }
 
-    /**
-     * Computes the hash code for this {@code Color}.
-     *
-     * @return a hash code value for this object.
-     * @since 1.0
-     */
-    public int hashCode() {
-        return value;
+    public static int HSBtoRGB(float hue, float saturation, float brightness) {
+        int r = 0, g = 0, b = 0;
+        if (saturation == 0) {
+            r = g = b = (int) (brightness * 255.0f + 0.5f);
+        } else {
+            float h = (hue - (float) Math.floor(hue)) * 6.0f;
+            float f = h - (float) java.lang.Math.floor(h);
+            float p = brightness * (1.0f - saturation);
+            float q = brightness * (1.0f - saturation * f);
+            float t = brightness * (1.0f - (saturation * (1.0f - f)));
+            switch ((int) h) {
+                case 0 -> {
+                    r = (int) (brightness * 255.0f + 0.5f);
+                    g = (int) (t * 255.0f + 0.5f);
+                    b = (int) (p * 255.0f + 0.5f);
+                }
+                case 1 -> {
+                    r = (int) (q * 255.0f + 0.5f);
+                    g = (int) (brightness * 255.0f + 0.5f);
+                    b = (int) (p * 255.0f + 0.5f);
+                }
+                case 2 -> {
+                    r = (int) (p * 255.0f + 0.5f);
+                    g = (int) (brightness * 255.0f + 0.5f);
+                    b = (int) (t * 255.0f + 0.5f);
+                }
+                case 3 -> {
+                    r = (int) (p * 255.0f + 0.5f);
+                    g = (int) (q * 255.0f + 0.5f);
+                    b = (int) (brightness * 255.0f + 0.5f);
+                }
+                case 4 -> {
+                    r = (int) (t * 255.0f + 0.5f);
+                    g = (int) (p * 255.0f + 0.5f);
+                    b = (int) (brightness * 255.0f + 0.5f);
+                }
+                case 5 -> {
+                    r = (int) (brightness * 255.0f + 0.5f);
+                    g = (int) (p * 255.0f + 0.5f);
+                    b = (int) (q * 255.0f + 0.5f);
+                }
+            }
+        }
+        return 0xff000000 | (r << 16) | (g << 8) | (b << 0);
     }
 
-    /**
-     * Determines whether another object is equal to this
-     * {@code Color}.
-     * <p>
-     * The result is {@code true} if and only if the argument is not
-     * {@code null} and is a {@code Color} object that has the same
-     * red, green, blue, and alpha values as this object.
-     *
-     * @param obj the object to test for equality with this
-     *            {@code Color}
-     * @return {@code true} if the objects are the same;
-     * {@code false} otherwise.
-     * @since 1.0
-     */
-    public boolean equals(Object obj) {
-        return obj instanceof Color && ((Color) obj).getRGB() == this.getRGB();
+    public static float[] RGBtoHSB(int r, int g, int b, float[] hsbvals) {
+        float hue, saturation, brightness;
+        if (hsbvals == null) {
+            hsbvals = new float[3];
+        }
+        int cmax = Math.max(r, g);
+        if (b > cmax) cmax = b;
+        int cmin = Math.min(r, g);
+        if (b < cmin) cmin = b;
+
+        brightness = ((float) cmax) / 255.0f;
+        if (cmax != 0)
+            saturation = ((float) (cmax - cmin)) / ((float) cmax);
+        else
+            saturation = 0;
+        if (saturation == 0)
+            hue = 0;
+        else {
+            float redc = ((float) (cmax - r)) / ((float) (cmax - cmin));
+            float greenc = ((float) (cmax - g)) / ((float) (cmax - cmin));
+            float bluec = ((float) (cmax - b)) / ((float) (cmax - cmin));
+            if (r == cmax)
+                hue = bluec - greenc;
+            else if (g == cmax)
+                hue = 2.0f + redc - bluec;
+            else
+                hue = 4.0f + greenc - redc;
+            hue = hue / 6.0f;
+            if (hue < 0)
+                hue = hue + 1.0f;
+        }
+        hsbvals[0] = hue;
+        hsbvals[1] = saturation;
+        hsbvals[2] = brightness;
+        return hsbvals;
     }
 
-    /**
-     * Returns a string representation of this {@code Color}. This
-     * method is intended to be used only for debugging purposes.  The
-     * content and format of the returned string might vary between
-     * implementations. The returned string might be empty but cannot
-     * be {@code null}.
-     *
-     * @return a string representation of this {@code Color}.
-     */
     public String toString() {
         return getClass().getName() + "[r=" + getRed() + ",g=" + getGreen() + ",b=" + getBlue() + "]";
     }
